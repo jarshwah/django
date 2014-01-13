4b78241b1322920466ffd667790f67de66fb4f01
Josh Smeaton <josh.smeaton@gmail.com>
2014-01-13 11:04:06 +1100

    Adding more tests for complex aggregations

--- tests/aggregation/tests.py
+++ tests/aggregation/tests.py
@@ -758,3 +758,15 @@ class ComplexAggregateTestCase(TestCase):
 
     def test_add_implementation(self):
         pass
+
+    def test_complex_values_aggregation(self):
+        max_rating = Book.objects.values('rating').aggregate(
+            double_max_rating=Max('rating')+Max('rating'))
+        self.assertEqual(max_rating['double_max_rating'], 5*2)
+
+        max_books_per_rating = Book.objects.values('rating').annotate(
+            books_per_rating=Count('id')+5
+        ).aggregate(Max('books_per_rating'))
+        self.assertEqual(
+            max_books_per_rating,
+            {'books_per_rating__max': 3+5})
